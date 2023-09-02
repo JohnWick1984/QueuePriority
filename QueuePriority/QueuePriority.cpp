0c42ab0780efc53e0ed7dbf08e783af43dd84661
Евгений <@EUGENE1984>
2023-09-02 12:26:23 +0500

    Добавьте файлы проекта.

---
+++ QueuePriority/QueuePriority.cpp
@@ -0,0 +1,99 @@
+﻿// QueuePriority.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+class QueuePriority 
+{
+public:
+    QueuePriority(int max_size) : max_size_(max_size), size_(0) 
+    {
+        items_ = new string[max_size];
+        priorities_ = new int[max_size];
+    }
+
+    ~QueuePriority()
+    {
+        delete[] items_;
+        delete[] priorities_;
+    }
+
+    void enqueue(const string& item, int priority) 
+    {
+        if (size_ < max_size_)
+        {
+            items_[size_] = item;
+            priorities_[size_] = priority;
+            size_++;
+        }
+        else 
+        {
+            cout << "Queue is full. Cannot enqueue." << endl;
+        }
+    }
+
+    string dequeue()
+    {
+        if (is_empty()) 
+        {
+            return "";
+        }
+
+        // Ищем элемент с наивысшим приоритетом
+        int highest_priority_index = 0;
+        for (int i = 1; i < size_; ++i)
+        {
+            if (priorities_[i] < priorities_[highest_priority_index])
+            {
+                highest_priority_index = i;
+            }
+        }
+
+        // Извлекаем элемент и его приоритет
+        string item = items_[highest_priority_index];
+
+        // Удаляем элемент и его приоритет из массивов
+        for (int i = highest_priority_index; i < size_ - 1; ++i) 
+        {
+            items_[i] = items_[i + 1];
+            priorities_[i] = priorities_[i + 1];
+        }
+        size_--;
+
+        return item;
+    }
+
+    bool is_empty() const
+    {
+        return size_ == 0;
+    }
+
+    int size() const 
+    {
+        return size_;
+    }
+
+private:
+    int max_size_;
+    int size_;
+    string* items_;
+    int* priorities_;
+};
+
+int main() {
+    // Создаем объект очереди с приоритетом (максимальный размер 100)
+    QueuePriority queue(100);
+
+    // Добавляем элементы с приоритетом
+    queue.enqueue("Task 1", 3);
+    queue.enqueue("Task 2", 1);
+    queue.enqueue("Task 3", 2);
+
+    // Извлекаем элементы из очереди в порядке убывания приоритета
+    cout << queue.dequeue() << endl; // Вывод: "Task 2"
+    cout << queue.dequeue() << endl; // Вывод: "Task 3"
+    cout << queue.dequeue() << endl; // Вывод: "Task 1"
+
+    return 0;
+}
---
+++ TaskStack/TaskStack.cpp
@@ -0,0 +1,113 @@
+﻿// TaskStack.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+class Task
+{
+public:
+    Task(const string& date, const string& description) : date(date), description(description) {}
+
+    string getDate() const
+    {
+        return date;
+    }
+
+    string getDescription() const
+    {
+        return description;
+    }
+
+private:
+    string date;
+    string description;
+};
+
+class TaskStack
+{
+public:
+    void push(const Task& task) 
+    {
+        tasks.push(task);
+    }
+
+    void pop() 
+    {
+        if (!tasks.empty()) 
+        {
+            tasks.pop();
+        }
+        else 
+        {
+            cout << "Stack is empty." << endl;
+        }
+    }
+
+    void listTasks() const 
+    {
+        cout << "\nTasks List:\n";
+        int index = 1;
+        stack<Task> tempStack = tasks;
+
+        while (!tempStack.empty()) 
+        {
+            Task task = tempStack.top();
+            cout << index << ". Data: " << task.getDate() << ", Description: " << task.getDescription() << endl;
+            tempStack.pop();
+            index++;
+        }
+    }
+
+private:
+    stack<Task> tasks;
+};
+
+int main()
+{
+    TaskStack taskStack;
+
+    while (true) 
+    {
+        cout << "\nMenu:\n";
+        cout << "1. Add Task\n";
+        cout << "2. Do the last task\n";
+        cout << "3. Exit\n";
+        cout << "Make a choice: ";
+
+        int choice;
+        cin >> choice;
+
+        if (choice == 1)
+        {
+            string date, description;
+            cout << "Enter data of task: ";
+            cin >> date;
+            cout << "Enter description of task: ";
+            cin.ignore();
+            getline(cin, description);
+
+            Task task(date, description);
+            taskStack.push(task);
+        }
+        else if (choice == 2)
+        {
+            taskStack.pop();
+        }
+        else if (choice == 3) 
+        {
+            break;
+        }
+        else 
+        {
+            cout << "Wrong choice! Try again!" << endl;
+        }
+
+        taskStack.listTasks();
+    }
+
+    return 0;
+}
